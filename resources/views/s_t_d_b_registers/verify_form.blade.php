a2910e8a3710cd338be1058d100f28e7fb4e63be
Britech <>
2022-01-05 09:41:47 +0800

    update ui verify form

--- resources/views/s_t_d_b_registers/verify_form.blade.php
+++ resources/views/s_t_d_b_registers/verify_form.blade.php
@@ -3,8 +3,10 @@
     <div class="card border-left-green border-left-6 card-content rounded-1 box-shadow-1 mt-3 p-2">
         {!! Form::open(['route' => 'sTDBRegisters.verified'], ['class' => 'form']) !!}
         <h3 class="font-weight-bold mb-1">Status Persil</h3>
-        <h6>Persil 1: <span class="badge bg-blue bg-lighten-2 mb-0-1">Clean and Clear</span></h6>
-        <h6>Persil 2: <span class="badge bg-warning bg-darken-2 mb-0-1">4% Masuk Kawasan Hutan (0.1 Ha)</span></h6>
+        @foreach($sTDBRegister->stdbDetailRegis as $key=>$item)
+            <h6>Persil {{$key+1}}: <span class="badge bg-blue bg-lighten-2 mb-0-1">Clean and Clear</span></h6>
+            {{--            <h6>Persil 2: <span class="badge bg-warning bg-darken-2 mb-0-1">4% Masuk Kawasan Hutan (0.1 Ha)</span></h6>--}}
+        @endforeach
         <!-- Message Field -->
         <div class="form-group">
             {!! Form::label('message', 'Catatan Sebelum verifikasi:') !!}
