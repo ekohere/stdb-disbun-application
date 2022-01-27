63b638c5169245d25880ebc8c4332b37ddbd903e
Eko Junirianto <eko.ilkom@gmail.com>
2022-01-27 09:10:51 +0800

    update

--- app/Console/Commands/Geo2shp.php
+++ app/Console/Commands/Geo2shp.php
@@ -54,6 +54,8 @@ class Geo2shp extends Command
                 $fileName = 'id_'.$persil->id.'_'.$persil->nama_peta."_np_".$persil->no_petak_peta;
                 $command="cd ".$pathTemp."; pgsql2shp -f ".$fileName.".shp -h ".env("DB_HOST")." -u ".env("DB_USERNAME_PG")." -P ".
                     env("DB_PASSWORD_PG")." -p ".env("DB_PORT_PG")." ".env("DB_DATABASE_PG").' "select * from polygon_persil where id='.$persil->polygon_persil_id.';"';
+
+                dd($command);
                 exec($command);
 
                 Storage::disk('public')->makeDirectory('shp_polygon');
