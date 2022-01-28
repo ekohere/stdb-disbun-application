f1bdc8f19130fc0b2d1aaf873287ab9164d0ad28
Britech <>
2022-01-28 16:22:13 +0800

    cek bug in view2

--- resources/views/s_t_d_b_registers/table.blade.php
+++ resources/views/s_t_d_b_registers/table.blade.php
@@ -37,10 +37,10 @@
             @endif
             <td>{!! $sTDBRegister->stdbDetailRegis->count() !!}</td>
             <td>
-                {{$sTDBRegister->latest_status->id}}
-                @if($sTDBRegister->latest_status->id==1)
+                {{$sTDBRegister->latest_status->name}}
+                @if($sTDBRegister->latest_status->id===1)
                     <div class="badge bg-warnig small mt-0-1"><i class="fa fa-clock-o"></i> {!! $sTDBRegister->latest_status->name !!}</div>
-                @elseif($sTDBRegister->latest_status->id==2)
+                @elseif($sTDBRegister->latest_status->id===2)
                     <div class="badge bg-green small mt-0-1"><i class="fa fa-check-circle"></i> {!! $sTDBRegister->latest_status->name !!}</div>
                 @endif
             </td>
