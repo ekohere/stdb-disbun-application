e5e7078f863c8e170f6f235efa6c01a06b5231b0
Britech <>
2022-01-30 16:53:40 +0800

    shp cek

--- app/Console/Commands/Geo2shp.php
+++ app/Console/Commands/Geo2shp.php
@@ -54,7 +54,7 @@ class Geo2shp extends Command
                 $fileName = 'id_'.$persil->id.'_'.str_replace(' ','-',$persil->nama_peta)."_np_".$persil->no_petak_peta;
                 $command="cd ".$pathTemp."; pgsql2shp -f ".$fileName.".shp -h ".env("DB_HOST")." -u ".env("DB_USERNAME_PG")." -P ".
                     env("DB_PASSWORD_PG")." -p ".env("DB_PORT_PG")." ".env("DB_DATABASE_PG").' "select * from polygon_persil where id='.$persil->polygon_persil_id.';"';
-
+                dd($command);
                 exec($command);
 
                 Storage::disk('public')->makeDirectory('shp_polygon');
