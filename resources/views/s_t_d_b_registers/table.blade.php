546c59841f25221f821ce684947eb5b285d28da6
Britech <>
2022-02-10 16:57:38 +0800

    update tampilan latest status model timeline fix

--- resources/views/s_t_d_b_registers/table.blade.php
+++ resources/views/s_t_d_b_registers/table.blade.php
@@ -52,11 +52,13 @@
                                     <span class="bg-info"><i class="fa fa-check"></i> {{$item->name}}</span>
                                 @endif
                                 <div>
-                                    <h6 class="mt-0-1 small text-bold-700">{{$item->stdbRegisHasStatus->created_at->format('d M Y h:m:s')}}</h6>
+                                    <h6 class="mt-0-1 mb-0 p-0 small text-bold-700">{{$item->stdbRegisHasStatus->created_at->format('d M Y h:m:s')}}</h6>
+                                    <h6 class="small mt-0 p-0">di review oleh: {{!empty($item->stdbRegisHasStatus->users_id)?$item->stdbRegisHasStatus->user->name:"-"}}</h6>
                                     <h6>
                                         Catatan:<br><b>{{!empty($item->stdbRegisHasStatus->message)?$item->stdbRegisHasStatus->message:"-"}}</b>
                                     </h6>
                                 </div>
+                                <hr class="m-0 p-0">
                             </li>
                             @endforeach
                         </ul>
