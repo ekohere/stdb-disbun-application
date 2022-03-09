738652341cc773afc804999cbbece58c4834853d
Britech <>
2022-03-10 07:05:30 +0800

    fix bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        @role('admin|PPR|KPH|admin_disbun')
+        @role('admin|PPR|KPH|BPN|admin_disbun')
             <div class="row">
             <div class="col-xl-3 col-lg-6 col-12">
                 <div class="card-content">
