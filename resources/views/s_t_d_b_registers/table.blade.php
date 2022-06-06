419c41e453d91845a1dc9949275174dcb43fede3
Britech <>
2022-06-06 08:47:10 +0800

    update bug created_at

--- resources/views/s_t_d_b_registers/table.blade.php
+++ resources/views/s_t_d_b_registers/table.blade.php
@@ -54,7 +54,7 @@
                                     <span class="bg-info"><i class="fa fa-check"></i> {{$item->name}}</span>
                                 @endif
                                 <div>
-                                    <h6 class="mt-0-1 mb-0 p-0 font-small-3 text-bold-700">{{$item->stdbRegisHasStatus->created_at->format('d M Y h:m:s')}}</h6>
+                                    <h6 class="mt-0-1 mb-0 p-0 font-small-3 text-bold-700">{{$item->pivot->created_at->format('d M Y h:m:s')}}</h6>
                                     <h6 class="small mt-0 p-0">di review oleh: <b>{{!empty($item->stdbRegisHasStatus->users_id)?$item->stdbRegisHasStatus->user->name:"-"}}</b></h6>
                                     <h6 class="mt-1">
                                         Catatan: <b><i>{{!empty($item->stdbRegisHasStatus->message)?$item->stdbRegisHasStatus->message:"-"}}</i></b>
